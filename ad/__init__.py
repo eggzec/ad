f7d93e1611822babed6effa9d11a869d8d7f02fd
saudzahirr <m.saud.zahir@gmail.com>
2026-05-10 18:18:33 +0500

    Fix version in __init__

--- ad/__init__.py
+++ ad/__init__.py
@@ -17,8 +17,14 @@ try:
 except ImportError:
     numpy_installed = False
 
-__version_info__ = (1, 3, 2)
-__version__ = ".".join(list(map(str, __version_info__)))
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 
 __author__ = "Abraham Lee"
 
